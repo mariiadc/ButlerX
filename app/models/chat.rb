313772673260975e4997eddfe7fbf0bf0236aa19
Mariia Campos <mariia.dcampos@gmail.com>
2020-03-03 09:10:31 +0000

    name of the chat model has been corrected

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,4 +1,4 @@
-class Booking < ApplicationRecord
+class Chat < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   belongs_to :user
