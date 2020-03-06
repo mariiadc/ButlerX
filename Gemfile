a8768bcba8630e36bd639186ca9461b856a5491d
Mariia Campos <mariia.dcampos@gmail.com>
2020-03-06 18:29:43 +0000

    fixing

--- Gemfile
+++ Gemfile
@@ -10,7 +10,6 @@ gem 'jbuilder', '~> 2.0'
 gem 'pg', '~> 0.21'
 gem 'puma'
 gem 'rails', '5.2.4.1'
-gem 'redis'
 gem 'sidekiq'
 gem 'sidekiq-failures', '~> 1.0'
 
--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module ButlerX
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-      config.active_job.queue_adapter = :sidekiq
+    config.active_job.queue_adapter = :sidekiq
 
   end
 end
