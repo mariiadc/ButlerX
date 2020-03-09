ad372065fb93d18cf8ebab83ab4988d3f54549ab
Mariia Campos <mariia.dcampos@gmail.com>
2020-03-09 11:00:58 +0000

    fixed chat?

--- app/controllers/guest/rooms_controller.rb
+++ app/controllers/guest/rooms_controller.rb
@@ -2,6 +2,7 @@ module Guest
   class RoomsController < ApplicationController
 
     def show
+      @chat_room = ChatRoom.find_by(booking_id: params[:booking_id])
 
       @booking = Booking.find(params[:booking_id])
        @room = Room.where(hotel_id: @booking.hotel_id)
