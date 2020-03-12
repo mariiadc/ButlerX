5877f989659b42ba7e98860b1b21ad5e636cbddb
Francisco Rosa <chicorosa@gmail.com>
2020-03-12 17:30:35 +0000

    modal meal

--- app/controllers/guest/meals_controller.rb
+++ app/controllers/guest/meals_controller.rb
@@ -7,12 +7,18 @@ module Guest
       @booking = Booking.find(params[:booking_id])
       @meals = Meal.where(hotel_id: @booking.hotel_id)
       @chat_room = ChatRoom.find_by(booking_id: params[:booking_id])
+      @cart_items = CartItem.all[-1..-2]
 
     end
 
     def show
       authorize @meal
       @chat_room = ChatRoom.find_by(booking_id: params[:booking_id])
+    end
+
+    def order_modal
+
+      binding.pry
     end
 
     private
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,21 @@
 import "bootstrap";
 // import "guest/bookings/show";
 import "../plugins/flatpickr";
+
+// const mealsIndex = document.querySelector('.meals.index');
+// if (mealsIndex) {
+//   const insertIntoModalMeal = () => {
+
+//   const addMeal = document.querySelector('.order-meal');
+//   const table = document.querySelector('.cart-table');
+//   addMeal.addEventListener('click', (event) => {
+//     event.preventDefault();
+//     let meal = `<tr class"cart-row"> <%= meal.name %> </tr>`
+//     table.insertAdjacentHTML("beforeend", meal);
+//   });
+
+// };
+
+//   insertIntoModalMeal();
+
+// }
