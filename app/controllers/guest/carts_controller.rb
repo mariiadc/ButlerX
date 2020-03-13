95bf7316e685142286919dc9cc5f97caaee53d15
Mariia Campos <mariia.dcampos@gmail.com>
2020-03-13 16:21:20 +0000

    findifdn

--- app/controllers/guest/carts_controller.rb
+++ app/controllers/guest/carts_controller.rb
@@ -4,6 +4,9 @@ module Guest
     before_action :set_cart
 
     def show
+    if @cart.state == 'paid'
+      @cart.empty!
+    end
     @cart_total_price = 0
       @cart.items.each do |i|
      @cart_total_price += i.price
@@ -27,15 +30,13 @@ module Guest
       session = Stripe::Checkout::Session.create(
         payment_method_types: ['card'],
         line_items: [line_items],
-        success_url: guest_booking_cart_url(@booking),
+        success_url: root_url,
         cancel_url: guest_booking_cart_url(@booking)
       )
 
       @cart.update(checkout_session_id: session.id)
 
-
-
-      redirect_to root_path, notice: 'Payment successfully processed.'
+      redirect_to pay_guest_booking_cart_path(@booking)
     end
 
     private
--- app/services/stripe_checkout_session_service.rb
+++ app/services/stripe_checkout_session_service.rb
@@ -1,6 +1,6 @@
 class StripeCheckoutSessionService
   def call(event)
-    bill = Bill.find_by(checkout_session_id: event.data.object.id)
-    bill.update(state: 'paid')
+    cart = Cart.find_by(checkout_session_id: event.data.object.id)
+    cart.update(state: 'paid')
   end
 end
